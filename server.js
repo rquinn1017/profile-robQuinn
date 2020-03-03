4524b7ecd374657106fa44d26c5a8cb97fa4038c
Robert Quinn <robertquinn@Roberts-Mac-mini.local>
2020-03-02 19:13:47 -0500

    wip to get using express

---
+++ server.js
@@ -0,0 +1,24 @@
+// Requiring necessary npm packages
+const express = require("express");
+const session = require("express-session");
+// Requiring passport as we've configured it
+
+
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+
