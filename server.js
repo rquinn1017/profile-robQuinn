73032d8833f27177ee511a38ae44f7ae367d40f7
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-03-02 21:17:04 -0500

    update html route

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.static("public"));
 
 
 // If no matching route is found default to home
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
   });
 
 
